aaa64aeedcf2f6b03c160bd501511ca87c6cf01d
Kamrun Nahar <knahar.kamrun@gmail.com>
2021-11-01 20:45:27 +0600

    fifth

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,18 +3,34 @@ import about from "../../Assets/about.jpg";
 
 const About = () => {
     return (
-        <div className="row d-flex align-items-center" style={{background: '#F2EEE9', marginRight:'0',marginBottom:'1vh', marginTop:'1vh'}}>
-
-            <div className="col-md-6 p-5">
-                    <h2 className="mb-5">About Us</h2>
-                    <p>We started as a small beauty studio in Islington, London. Our main idea was to create the best beauty studio in the world. Can there be compromises in the best studio in the world? Our answer is always no, we care about the best quality, we hire the best specialists and provide the best customer service. This approach allowed us to grow and create awesome team that is passionate about everything we do.</p>
-            </div>
-
-            <div className="col-md-6">
-                <img src={about} style={{width:'100%', height:"75vh"}} alt="About"/>
-            </div>
-            
+      <div
+        className="row d-flex align-items-center"
+        style={{
+          background: "#F2EEE9",
+          marginRight: "0",
+          marginBottom: "1vh",
+          marginTop: "1vh",
+        }}
+      >
+        <div className="col-md-6 p-5">
+          <h2 className="mb-5">About Us</h2>
+          <p>
+            As the leading international organization in the field of tourism,
+            UNWTO promotes tourism as a driver of economic growth, inclusive
+            development and environmental sustainability and offers leadership
+            and support to the sector in advancing knowledge and tourism
+            policies worldwide
+          </p>
         </div>
+
+        <div className="col-md-6">
+          <img
+            src={about}
+            style={{ width: "100%", height: "75vh" }}
+            alt="About"
+          />
+        </div>
+      </div>
     );
 };
 
